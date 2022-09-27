e3d228ae6d79f80ccdcaf6604c6b5586c5723475
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-09-27 23:24:43 +0700

    Implement feature/footer

--- src/Containers/HeaderFooter/components/Footer/index.js
+++ src/Containers/HeaderFooter/components/Footer/index.js
@@ -1,14 +1,59 @@
 import React from 'react'
 import * as S from './styles'
+import LogoInsta from '../../../../assets/FooterLogoInsta.svg'
+import LogoTwitter from '../../../../assets/FooterLogoTwitter.svg'
+import LogoFb from '../../../../assets/FooterLogoFb.svg'
+import LogoYoutube from '../../../../assets/FooterLogoYoutube.svg'
 
 function Footer() {
   return (
     <S.ContainerFooter>
       <S.Footer>
-        <S.FooterItems></S.FooterItems>
-        <S.FooterItems></S.FooterItems>
-        <S.FooterItems></S.FooterItems>
-        <S.FooterItems></S.FooterItems>
+        <S.FooterItems>
+          <S.FooterService>ABOUT US</S.FooterService>
+          <S.FooterDes>
+            Nepal's Coolest Eyewear Store
+            <p>
+              ORDER ANYTIME AND RECEIVE FREE AND FAST DELIVERY.
+            </p>
+          </S.FooterDes>
+        </S.FooterItems>
+
+        <S.FooterItems>
+          <S.FooterService>POLICY</S.FooterService>
+          <div className='Sub-Items'>
+            <S.FooterDes>
+              Delivery
+              <p>Exchange and Return</p>
+            </S.FooterDes>
+            <S.FooterDes>
+              Payment
+              <p>Cancellation</p>
+            </S.FooterDes>
+          </div>
+        </S.FooterItems>
+
+        <S.FooterItems>
+          <S.FooterService>CONTACT</S.FooterService>
+          <S.FooterDes>
+            <S.FooterDesList>
+              <li>Phone <span>9823-749-232</span></li>
+              <li>Email <span>visioncustomercare@outlook.com</span></li>
+              <li>Address <span>Maitidevi, Kathmandu</span></li>
+            </S.FooterDesList>
+          </S.FooterDes>
+        </S.FooterItems>
+
+        <S.FooterItems>
+          <S.FooterService>SOCIALS</S.FooterService>
+          <S.FooterIcon>
+            <img src={LogoInsta} alt="" />
+            <img src={LogoTwitter} alt="" />
+            <img src={LogoFb} alt="" />
+            <img src={LogoYoutube} alt="" />
+          </S.FooterIcon>
+        </S.FooterItems>
+
       </S.Footer>
     </S.ContainerFooter>
   )
--- src/Containers/HeaderFooter/components/Footer/styles.js
+++ src/Containers/HeaderFooter/components/Footer/styles.js
@@ -1,5 +1,72 @@
 import styled from "styled-components";
 
-export const ContainerFooter = styled.div``
-export const Footer = styled.div``
-export const FooterItems = styled.div``
+export const ContainerFooter = styled.div`
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  background-color: #000;
+  color: #fff;
+  height: 240px;
+  padding: 30px 156px 0;
+`
+
+export const Footer = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 5fr 5fr;
+  grid-row-gap: 9.8px;
+`
+
+export const FooterItems = styled.div`
+  font-weight: 400;
+  font-size: 15px;
+  line-height: 22px;
+
+  &:last-child {
+    display: flex;
+    align-items: center;
+  }
+
+  .Sub-Items {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    /* padding-right: 17rem; */
+  }
+
+  p {
+    margin: 0;
+    margin-top: 3px
+  }
+`
+
+export const FooterService = styled.div`
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 27px;
+`
+
+export const FooterDes = styled.div`
+  margin-top: 9px;  
+  
+`
+export const FooterDesList = styled.ul`
+  display: flex;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  li {
+    list-style: none;
+      span {
+      font-weight: 300;
+      font-size: 15px;
+      padding-left: 21px;
+    }
+  }
+`
+
+export const FooterIcon = styled.div`
+  img {
+    margin-left: 21px;
+  }
+`
