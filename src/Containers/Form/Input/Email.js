fd1c26f6c0ab03775b30ebb1e572d0435032bc3f
Thanh Nhan <nguyenthanhnhan12701@gmail.com>
2022-09-28 21:05:31 +0700

    Implement feature/sign in

---
+++ src/Containers/Form/Input/Email.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+import Mail from '../../../assets/SignInIconMail.svg'
+import Lock from '../../../assets/SignInIconLock.svg'
+import SignInBg from '../../../assets/SignInBg.png'
+import Logo from '../../../../assets/HeaderLogo.png'
+
+
+const ContainersSignIn = styled.div``
+const SignIn = styled.div``
+
+function Email() {
+  return (
+    <ContainersSignIn>
+      <SignIn>
+        
+      </SignIn>
+    </ContainersSignIn>
+  )
+}
+
+export default Email
